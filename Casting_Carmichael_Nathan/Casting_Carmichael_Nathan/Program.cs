33903ef174c28e8b39792801b4c603e730659903
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 15:08:30 -0500

    Code using Casting

---
+++ Casting_Carmichael_Nathan/Casting_Carmichael_Nathan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Casting_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user a question//
+            Console.WriteLine("What is your favorite number?");
+            //The user input//
+            int favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            //Displaying the users response//
+            Console.WriteLine($"WOW,Your favorite number is {favoriteNumber}!");
+            //Asks the user a question//
+            Console.WriteLine("So do you like video games? must put true or false");
+            //The user input//
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+            //Displaying the users response//
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+        }
+    }
+}
