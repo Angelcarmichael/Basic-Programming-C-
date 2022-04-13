bbd8a55409b89708c32d9fe9478af872c5cd13a9
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 15:09:56 -0500

    First Ever Program

---
+++ HelloWorld_Carmichael_Nathan/HelloWorld_Carmichael_Nathan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Asks a question//
+            Console.WriteLine("What is your first name?");
+            //Allows the user to input imformation//
+            string firstName = Console.ReadLine();
+            //Asks a question//
+            Console.WriteLine("What is your last name?");
+            //Allows the user to input imformation//
+            string lastName = Console.ReadLine();
+            //Displace Hi and what the user entered//
+            Console.WriteLine($"Hi {firstName} {lastName}");
+        }
+    }
+}
