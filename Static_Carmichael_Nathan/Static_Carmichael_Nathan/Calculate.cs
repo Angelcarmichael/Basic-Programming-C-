bf578109457b7e048372be3b64927dada705ff43
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 14:57:44 -0500

    Code using Static Members

---
+++ Static_Carmichael_Nathan/Static_Carmichael_Nathan/Calculate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Carmichael_Nathan
+{
+    //Creates a static class
+    static class Calculate
+    {
+        //A public static float
+        public static float result;
+
+        //A static constructor that initializes the result value to '0.0f
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        //A method that Adds two whole numbers  
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        //A method that Subtracts two whole numbers
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        //A method that Multiplys two whole numbers
+        public static int Multiply(int number1, int number2)
+        {
+            return number1 * number2; 
+        }
+        //A method that Divides two whole numbers
+        public static int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        //A method that Adds two decimal numbers
+        public static float Add(float float1, float float2)
+        {
+            return float1 + float2;
+        }
+        //A method that Subtracts two decimal numbers
+        public static float Subtract(float float1, float float2)
+        {
+            return float1 - float2;
+        }
+        //A method that Multiplys two decimal numbers
+        public static float Multiply(float float1, float float2)
+        {
+            return float1 * float2;
+        }
+        //A method that Divides two decimal numbers
+        public static float Divide(float float1, float float2)
+        {
+            return float1 / float2;
+        }
+    }
+}
---
+++ Static_Carmichael_Nathan/Static_Carmichael_Nathan/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Static_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the  user to enter a whole number
+            Console.WriteLine($"Please enter a whole number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            //Asks the  user to enter anothor whole number
+            Console.WriteLine($"Please enter anothor whole number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            //Calls the Add, Subtract, Multiply, and Divide methods 
+            Calculate.Add(number1, number2);
+            Calculate.Subtract(number1, number2);
+            Calculate.Multiply(number1, number2);
+            Calculate.Divide(number1, number2);
+
+            //Asks the  user to enter a decimal number
+            Console.WriteLine($"Please enter a decimal number");
+            float float1 = Convert.ToSingle(Console.ReadLine());
+            //Asks the  user to enter anothor decimal number
+            Console.WriteLine($"Please enter a anothor decimal number");
+            float float2 = Convert.ToSingle(Console.ReadLine());
+
+            //Calls the Add, Subtract, Multiply, and Divide methods
+            Calculate.Add(float1, float2);
+            Calculate.Subtract(float1, float2);
+            Calculate.Multiply(float1, float2);
+            Calculate.Divide(float1, float2);
+
+            //Tells the user the results for the whole numbers they enter, after the numbers have been Added, Subtracted, Multiplyed, and Divided together 
+            Console.WriteLine($"{number1} + {number2} =");
+            Console.WriteLine(Calculate.Add(number1, number2));
+            Console.WriteLine($"{number1} - {number2} =");
+            Console.WriteLine(Calculate.Subtract(number1, number2));
+            Console.WriteLine($"{number1} * {number2} =");
+            Console.WriteLine(Calculate.Multiply(number1, number2));
+            Console.WriteLine($"{number1} / {number2} =");
+            Console.WriteLine(Calculate.Divide(number1, number2));
+
+            //Tells the user the results for the decimal numbers they enter, after the numbers been Added, Subtracted, Multiplyed, and Divided together
+            Console.WriteLine($"{float1} + {float2} =");
+            Console.WriteLine(Calculate.Add(float1, float2));
+            Console.WriteLine($"{float1} - {float2} =");
+            Console.WriteLine(Calculate.Subtract(float1, float2));
+            Console.WriteLine($"{float1} * {float2} =");
+            Console.WriteLine(Calculate.Multiply(float1, float2));
+            Console.WriteLine($"{float1} / {float2} =");
+            Console.WriteLine(Calculate.Divide(float1, float2));
+        }
+    }
+}
