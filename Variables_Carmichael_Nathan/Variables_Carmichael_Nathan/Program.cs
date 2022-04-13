5995316d89a59afe663afa662f5f25c347fbee27
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 15:06:06 -0500

    Code using Variables

---
+++ Variables_Carmichael_Nathan/Variables_Carmichael_Nathan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Variables_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //Place int and add value//
+            int favoriteName = 18;
+            //Place bool and add value//
+            bool isJumping = false, isRuning = false;
+            //Place float and add value//
+            float nate = 2.5f;
+            //Place const double and add value//
+            const double finalGrade = 1.5;
+            //Displaying favoriteName to console//
+            Console.WriteLine(favoriteName);
+            //Displaying isJumping to console//
+            Console.WriteLine(isJumping);
+            //Displaying isRuning to console//
+            Console.WriteLine(isRuning);
+            //Displaying nate to console//
+            Console.WriteLine(nate);
+            //Displaying finalGrade to console//
+            Console.WriteLine(finalGrade);
+        }
+    }
+}
