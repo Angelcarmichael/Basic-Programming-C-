775f13d8f415f536abd398a989d48145b7464d85
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 15:00:52 -0500

    Code using Classes

---
+++ Classes_Carmichael_Nathan/Classes_Carmichael_Nathan/Greetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes_Carmichael_Nathan
+{
+    class Greetings
+    {
+        //A method that welcomes the user
+        public void Welcome()
+        {
+            //Displays a message thet welcomes the user
+            Console.WriteLine($"Hello, who are you?");
+        }
+        //A method that greets and thanks the user 
+        public void Hello(string name)
+        {
+            //Displays a message thet greets and thanks the user
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+    }
+}
---
+++ Classes_Carmichael_Nathan/Classes_Carmichael_Nathan/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates an instance of the Greetings class
+            Greetings g = new Greetings();
+            //Calls the Welcome method
+            g.Welcome(); 
+            //Asks the user to enter a name 
+            Console.WriteLine($"Please enter a name");
+            string name = Console.ReadLine();
+            //Calla the Hello method
+            g.Hello(name);
+        }
+    }
+       
+}
