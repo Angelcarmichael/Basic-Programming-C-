6719e97c82844809700b579ea9bca82fdf2e64d7
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 14:58:40 -0500

    Code using Constructors

---
+++ Constructors_Carmichael_Nathan/Constructors_Carmichael_Nathan/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_Carmichael_Nathan
+{
+    class Game
+    {
+        //Two public string fields
+        public string title;
+        public string genre;
+        //Creates a class constructor
+        public Game(string title = "Unknown Title", string genre = "Casual")
+        {
+            this.title = title;
+            this.genre = genre;
+        }
+    }
+}
---
+++ Constructors_Carmichael_Nathan/Constructors_Carmichael_Nathan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Constructors_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displays a message to the user 
+            Console.WriteLine($"Game 1's title and genre");
+            //Creates an instance of the Game class
+            Game g = new Game("LEGO DC Super-Villains", "Action-adventure platform");
+            //Tells the user the game's title 
+            Console.WriteLine(g.title);
+            //Tells the user the game's genre 
+            Console.WriteLine(g.genre);
+            //Displays a message to the user
+            Console.WriteLine($"Game 2's title and genre");
+            //Creates an instance of the Game class
+            Game g2 = new Game();
+            //Tells the user the game's title
+            Console.WriteLine(g2.title);
+            //Tells the user the game's genre
+            Console.WriteLine(g2.genre);
+        }
+    }
+}
