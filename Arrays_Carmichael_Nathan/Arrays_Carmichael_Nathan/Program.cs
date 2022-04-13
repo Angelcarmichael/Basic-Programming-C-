2a60f5a8e6662514cb114d814cb091b6eabaef5e
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-04-13 15:01:49 -0500

    Code using Arrays

---
+++ Arrays_Carmichael_Nathan/Arrays_Carmichael_Nathan/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Arrays_Carmichael_Nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks the user to enter enter the numbers elements
+            Console.WriteLine($"Please enter numbers elements");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            //Makes an int array 
+            int[] intarray = new int[numbers];
+            //integers for the first foreach loop
+            int sum = 0;
+            int index = 0;
+            //Loops through the numbers to find the sum
+            foreach (int number in intarray)
+            {
+                //Asks the user to enter a number
+                Console.WriteLine($"Please enter a number");
+                intarray [index] = Convert.ToInt32(Console.ReadLine());
+                sum += intarray [index];
+                index++;
+            }
+            //Tells the user the sum of the numbers they entered 
+            Console.WriteLine($"The sum is {sum}");
+            //Sorts the numbrs in numerical order
+            Array.Sort(intarray);
+            //Tells the user the numerical order of the numbers
+            Console.WriteLine($"The numerical order of the numbers is");
+            //Loops through the numbers to find the numerical order of the numbers
+            foreach (int number in intarray)
+            {
+                //Tells the user the numerical order of the numbers
+                Console.WriteLine(number);
+            }
+                 
+                   
+        }
+    }
+}
